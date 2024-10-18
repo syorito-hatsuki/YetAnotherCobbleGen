6b88c57503a1f96a62e347f0f456d3e5e503fab0
syorito-hatsuki <enderdron3@gmail.com>
2024-10-18 20:46:34 +0300

    Added folder creating if not exist for default folders

--- src/main/kotlin/dev/syoritohatsuki/yacg/util/PathUtil.kt
+++ src/main/kotlin/dev/syoritohatsuki/yacg/util/PathUtil.kt
@@ -4,11 +4,18 @@ import dev.syoritohatsuki.yacg.config.GeneratorsManager
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.util.Identifier
 import java.nio.file.Path
+import kotlin.io.path.createDirectory
+import kotlin.io.path.notExists
 
 object PathUtil {
 
-    private val yacgRootPath: Path = FabricLoader.getInstance().configDir.resolve("yacg")
-    val generatorsPath: Path = yacgRootPath.resolve("generators")
+    private val yacgRootPath: Path = FabricLoader.getInstance().configDir.resolve("yacg").apply {
+        if (notExists()) createDirectory()
+    }
+
+    val generatorsPath: Path = yacgRootPath.resolve("generators").apply {
+        if (notExists()) createDirectory()
+    }
 
     fun getNativeImagePath(id: Identifier, side: String): Path =
         generatorsPath.resolve(id.namespace).resolve(id.path).resolve("textures").resolve("$side.png")
